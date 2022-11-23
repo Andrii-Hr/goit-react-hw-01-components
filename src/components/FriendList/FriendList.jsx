b649df9ba25b58045f3fa9e10c2380dc773a4c55
Andrii-Hr <andriihryb@ukr.net>
2022-11-23 14:08:45 +0200

    add 3 task

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,10 +1,35 @@
-import PropTypes from 'prop-types'
-import css from '../FriendList/FriendList.module.css'
+import PropTypes from 'prop-types';
+import css from '../FriendList/FriendList.module.css';
 
-export const FriendList = ({friend}) => {
-    return (
-        <section className={css.friendList}>
-    let isOnline = false
+export const FriendList = ({ friends }) => {
+  return (
+    <section className={css.friends}>
+      <ul className={css.friends__list}>
+        {friends.map(({ avatar, name, isOnline, id }) => {
+          return (
+            <li className={css.item} key={id}>
+              {isOnline === true ? (
+                <span className={css.status}>{isOnline}</span>
+              ) : (
+                <span className={css.status_online}>{isOnline}</span>
+              )}
+              <img
+                className={css.avatar}
+                src={avatar}
+                alt="User avatar"
+                width="48"
+              />
+              <p className={css.name}>{name}</p>
+            </li>
+          );
+        })}
+      </ul>
     </section>
-    )
-}
+  );
+};
+
+FriendList.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired
+};
